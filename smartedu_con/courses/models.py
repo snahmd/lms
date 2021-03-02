11f3427247475ddf9acf9b0a113ce4cfc1d6da83
snahmd <sanahmed7@gmail.com>
2021-03-02 23:53:33 +0100

    first model

---
+++ smartedu_con/courses/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=200, unique=True, verbose_name="Kurs Adı", help_text="Kurs Adını Yazınız.")
+    description = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="courses/%Y/%m/%d/", default="courses/abc.jpg")
+    date = models.DateTimeField(auto_now=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
--- smartedu_con/smartedu/settings.py
+++ smartedu_con/smartedu/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'courses',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
